2b11ff3df48085be251fe581995e919fda4014ee
ildar <ildar.icoosoft@gmail.com>
2020-08-29 05:37:50 +0500

    interm

--- src/components/OngoingGamesContainer.tsx
+++ src/components/OngoingGamesContainer.tsx
@@ -1,10 +1,42 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { GamePreviewsList } from "./GamePreviewsList";
+import Game from "../interfaces/Game";
 
 export interface OngoingGamesContainerProps {
   [key: string]: any;
 }
 
 export const OngoingGamesContainer: FC<OngoingGamesContainerProps> = () => {
-  return <GamePreviewsList />;
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    const timeoutId: NodeJS.Timeout = setTimeout(() => {
+      setGames([
+        {
+          id: 1,
+          initialFen: "startpos",
+          wtime: 300000,
+          btime: 300000,
+          moves: "",
+          status: "started",
+          white: null,
+          black: null,
+        },
+        {
+          id: 2,
+          initialFen: "rnbqkbnr/8/8/8/8/8/8/RNBQKBNR w KQkq - 0 1",
+          wtime: 300000,
+          btime: 300000,
+          moves: "",
+          status: "started",
+          white: null,
+          black: null,
+        },
+      ]);
+    }, 3000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return <GamePreviewsList games={games} />;
 };
--- src/components/__tests__/OngoingGamesContainer.test.tsx
+++ src/components/__tests__/OngoingGamesContainer.test.tsx
@@ -3,6 +3,32 @@ import React from "react";
 import mountTest from "../../tests/mountTest";
 import { OngoingGamesContainer } from "../OngoingGamesContainer";
 import { GamePreviewsList } from "../GamePreviewsList";
+import Game from "../../interfaces/Game";
+
+jest.useFakeTimers();
+
+const games: Game[] = [
+  {
+    id: 1,
+    initialFen: "startpos",
+    wtime: 300000,
+    btime: 300000,
+    moves: "",
+    status: "started",
+    white: null,
+    black: null,
+  },
+  {
+    id: 2,
+    initialFen: "rnbqkbnr/8/8/8/8/8/8/RNBQKBNR w KQkq - 0 1",
+    wtime: 300000,
+    btime: 300000,
+    moves: "",
+    status: "started",
+    white: null,
+    black: null,
+  },
+];
 
 describe("OngoingGamesContainer", () => {
   mountTest(OngoingGamesContainer);
@@ -13,6 +39,25 @@ describe("OngoingGamesContainer", () => {
       const testInstance = testRenderer.root;
 
       expect(testInstance.findAllByType(GamePreviewsList).length).toBe(1);
+    });
+  });
+
+  describe("children components props", () => {
+    describe("GamePreviewsList", () => {
+      it("games", () => {
+        const testRenderer = TestRenderer.create(<OngoingGamesContainer />);
+        const testInstance = testRenderer.root;
+
+        const gamePreviewsComponent = testInstance.findByType(GamePreviewsList);
+
+        expect(gamePreviewsComponent.props.games).toEqual([]);
+
+        TestRenderer.act(() => {
+          jest.runAllTimers();
+        });
+
+        expect(gamePreviewsComponent.props.games).toEqual(games);
+      });
     });
   });
 });
