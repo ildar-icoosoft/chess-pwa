5577f38b924ad2dc32be9ee49647e42bb87c3c5b
ildar <ildar.icoosoft@gmail.com>
2020-08-30 23:31:55 +0500

    interm

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import TestRenderer from "react-test-renderer";
 import { render } from "@testing-library/react";
 import App from "./App";
+import mountTest from "./tests/mountTest";
+import HomePage from "./pages/HomePage";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Game/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  mountTest(App);
+
+  it("Snapshot", () => {
+    const tree = TestRenderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  describe("children components", () => {
+    it("contains HomePage", () => {
+      const testRenderer = TestRenderer.create(<App />);
+      const testInstance = testRenderer.root;
+
+      expect(testInstance.findAllByType(HomePage).length).toBe(1);
+    });
+  });
+
+  describe("DOM structure", () => {
+    it("renders learn react link", () => {
+      const { getByText } = render(<App />);
+      const linkElement = getByText(/Game/i);
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
 });
