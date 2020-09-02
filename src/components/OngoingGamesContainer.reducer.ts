de9bf1a4310d9f1d49156c9e0d3e5af5c0aceb90
ildar <ildar.icoosoft@gmail.com>
2020-09-02 09:21:42 +0500

    interm

--- src/components/OngoingGamesContainer.reducer.ts
+++ src/components/OngoingGamesContainer.reducer.ts
@@ -1,3 +1,4 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { findIndex } from "lodash";
 import Game from "../interfaces/Game";
 import { SubscriptionData } from "../interfaces/SubscriptionData";
@@ -11,47 +12,49 @@ export type Action =
   | { type: "UPDATE_GAME"; payload: SubscriptionData }
   | { type: "CREATE_GAME"; payload: SubscriptionData };
 
-export const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case "GET_GAMES":
-      return {
-        games: action.payload,
-      };
-    case "UPDATE_GAME": {
-      const index: number = findIndex(state.games, {
-        id: action.payload.id,
-      });
+const getGames = (state: State, { payload }: Action): State => ({
+  games: payload as Game[],
+});
 
-      if (index !== -1) {
-        return {
-          games: state.games.map((item, itemIndex) => {
-            if (itemIndex === index) {
-              return {
-                ...action.payload.previous,
-                ...action.payload.data,
-              };
-            }
-            return item;
-          }),
-        };
-      }
+const updateGame = (state: State, { payload }: Action): State => {
+  const subscriptionData = payload as SubscriptionData;
 
-      return {
-        games: [
-          ...state.games,
-          {
-            ...action.payload.previous,
-            ...action.payload.data,
-          },
-        ],
-      };
-    }
-    case "CREATE_GAME": {
-      return {
-        games: [...state.games, action.payload.data],
-      };
-    }
-    default:
-      throw new Error(`unknown action type ${(action as any).type}`);
+  const game = {
+    ...subscriptionData.previous,
+    ...subscriptionData.data,
+  };
+
+  const index: number = findIndex(state.games, {
+    id: subscriptionData.id,
+  });
+
+  if (index !== -1) {
+    return {
+      games: state.games.map((item, itemIndex) => {
+        if (itemIndex === index) {
+          return game;
+        }
+        return item;
+      }),
+    };
   }
+
+  return {
+    games: [...state.games, game],
+  };
 };
+
+const createGame = (state: State, { payload }: Action): State => ({
+  games: [...state.games, (payload as SubscriptionData).data],
+});
+
+export const reducer = createReducer<State>(
+  {
+    games: [],
+  },
+  {
+    GET_GAMES: getGames,
+    UPDATE_GAME: updateGame,
+    CREATE_GAME: createGame,
+  }
+);
