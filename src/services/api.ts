005f539b384ef6ea5b1b6d97ccba89aab5fc3d80
ildar <ildar.icoosoft@gmail.com>
2020-09-03 05:18:55 +0500

    feat: handle API errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,22 @@
+import { JWR } from "sails.io.js";
 import Game from "../interfaces/Game";
 import ioClient from "./ioClient";
 import { SubscriptionData } from "../interfaces/SubscriptionData";
 
 export const getOngoingGames = (): Promise<Game[]> => {
-  return new Promise((resolve) => {
-    ioClient.socket.get("/api/v1/game/playing", (body: Game[]) => {
-      resolve(body);
+  return new Promise((resolve, reject) => {
+    ioClient.socket.get("/api/v1/game/playing", (body: Game[], jwr: JWR) => {
+      if (jwr.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(jwr);
+      }
     });
   });
 };
 
 export const watchGames = (cb: (data: SubscriptionData) => void): void => {
-  ioClient.socket.on("game", (msg: SubscriptionData) => {
+  ioClient.socket.on("gamehhhh", (msg: SubscriptionData) => {
     cb(msg);
   });
 };
