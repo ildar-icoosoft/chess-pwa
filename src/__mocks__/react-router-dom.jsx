309e0736da57e21a7e6d5da78f99cf28a8e967a9
ildar <ildar.icoosoft@gmail.com>
2020-09-05 07:10:36 +0500

    interm

--- src/__mocks__/react-router-dom.jsx
+++ src/__mocks__/react-router-dom.jsx
@@ -6,7 +6,7 @@ import rrd from "react-router-dom";
 // @see https://medium.com/@antonybudianto/react-router-testing-with-jest-and-enzyme-17294fefd303
 // @see https://stackoverflow.com/questions/51031761/how-to-mock-browserrouter-of-react-router-dom-using-jest
 // Just render plain div with its children
-// rrd.BrowserRouter = ({ children }: any) => <div>{children}</div>;
-rrd.Link = ({ children }: any) => <>{children}</>;
+// rrd.BrowserRouter = ({ children }) => <div>{children}</div>;
+rrd.Link = ({ children }) => <>{children}</>;
 
 module.exports = rrd;
