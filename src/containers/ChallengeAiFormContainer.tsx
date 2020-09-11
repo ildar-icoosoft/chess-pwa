44357801a7d6d40596316c7b4d1ce0a8a02312b4
ildar <ildar.icoosoft@gmail.com>
2020-09-11 12:01:30 +0500

    interm

--- src/containers/ChallengeAiFormContainer.tsx
+++ src/containers/ChallengeAiFormContainer.tsx
@@ -1,21 +1,13 @@
 import React, { FC } from "react";
 import { ChallengeAiForm } from "../components/ChallengeAiForm";
 import { challengeAi as apiChallengeAi } from "../services/api";
+import { ChallengeAiData } from "../interfaces/ChallengeAiData";
 
 export const ChallengeAiFormContainer: FC<unknown> = () => {
-  const challengeAi = () => {
-    apiChallengeAi({
-      level: 3,
-      color: "random",
-      clockLimit: 400,
-      clockIncrement: 4,
-    })
-      .then((res) => {
-        debugger;
-      })
-      .catch((err) => {
-        debugger;
-      });
+  const challengeAi = (data: ChallengeAiData) => {
+    return apiChallengeAi(data)
+      .then((res) => {})
+      .catch((err) => {});
   };
 
   return <ChallengeAiForm onSubmit={challengeAi} />;
