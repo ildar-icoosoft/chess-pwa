67f591af1cc7bd49db16a9684e0a59128be88ea3
ildar <ildar.icoosoft@gmail.com>
2020-09-13 17:30:30 +0500

    interm

--- src/redux/interfaces/NormalizedGameEntity.ts
+++ src/redux/interfaces/NormalizedGameEntity.ts
@@ -1,3 +1,7 @@
 import Game from "../../interfaces/Game";
 
-export default interface NormalizedGameEntity extends Game {}
+export default interface NormalizedGameEntity
+  extends Omit<Game, "white" | "black"> {
+  white: number;
+  black: number;
+}
