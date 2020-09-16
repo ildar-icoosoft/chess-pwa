58fcbbd251e9393162b02a11bf596909ac7a6e90
ildar <ildar.icoosoft@gmail.com>
2020-09-16 18:00:12 +0500

    ongoingGamesContainer test

--- src/containers/__tests__/OngoingGamesContainer.test.tsx
+++ src/containers/__tests__/OngoingGamesContainer.test.tsx
@@ -4,72 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import OngoingGamesContainer from "../OngoingGamesContainer";
 import { GamePreviewsList } from "../../components/GamePreviewsList";
 import mountTest from "../../test-utils/mountTest";
-import { RootState } from "../../app/rootReducer";
 import { fetchOngoingGames } from "../../redux/slices/ongoingGamesSlice";
+import {
+  defaultState,
+  stateWithOngoingGames,
+} from "../../test-utils/data-sample/state";
 
 jest.useFakeTimers();
 
 jest.mock("../../redux/slices/ongoingGamesSlice");
 
-const stateSample1: RootState = {
-  currentUser: {
-    userId: null,
-    isLoading: false,
-    error: null,
-  },
-  authModal: {
-    isAuthModalVisible: false,
-  },
-  challengeAiModal: {
-    isChallengeAiModalVisible: false,
-  },
-  ongoingGames: {
-    items: [],
-    isLoading: false,
-    error: null,
-  },
-  entities: {
-    users: {},
-    games: {},
-  },
-};
-
-const stateSample2: RootState = {
-  currentUser: {
-    userId: null,
-    isLoading: false,
-    error: null,
-  },
-  authModal: {
-    isAuthModalVisible: false,
-  },
-  challengeAiModal: {
-    isChallengeAiModalVisible: false,
-  },
-  ongoingGames: {
-    items: [1],
-    isLoading: false,
-    error: null,
-  },
-  entities: {
-    users: {},
-    games: {
-      "1": {
-        id: 1,
-        initialFen: "startpos",
-        wtime: 300000,
-        btime: 300000,
-        moves: "",
-        status: "started",
-        white: null,
-        black: null,
-      },
-    },
-  },
-};
-
 describe("OngoingGamesContainer", () => {
   beforeEach(() => {
+    (useSelector as jest.Mock).mockImplementation((cb) => cb(defaultState));
     useDispatch<jest.Mock>().mockClear();
     (useEffect as jest.Mock).mockReset();
   });
@@ -88,8 +35,6 @@ describe("OngoingGamesContainer", () => {
   describe("children components props", () => {
     describe("GamePreviewsList", () => {
       it("games", () => {
-        (useSelector as jest.Mock).mockImplementation((cb) => cb(stateSample1));
-
         const testRenderer = TestRenderer.create(<OngoingGamesContainer />);
         const testInstance = testRenderer.root;
 
@@ -97,7 +42,9 @@ describe("OngoingGamesContainer", () => {
 
         expect(gamePreviewsComponent.props.games).toEqual([]);
 
-        (useSelector as jest.Mock).mockImplementation((cb) => cb(stateSample2));
+        (useSelector as jest.Mock).mockImplementation((cb) =>
+          cb(stateWithOngoingGames)
+        );
 
         testRenderer.update(<OngoingGamesContainer />);
 
@@ -118,9 +65,7 @@ describe("OngoingGamesContainer", () => {
   });
 
   describe("dispatch() calls", () => {
-    it("fetchOngoingGames()", () => {
-      (useSelector as jest.Mock).mockImplementation((cb) => cb(stateSample1));
-
+    it("should call dispatch(fetchOngoingGames())", () => {
       const dispatch = useDispatch<jest.Mock>();
 
       (useEffect as jest.Mock).mockImplementationOnce((cb) => cb());
@@ -131,9 +76,7 @@ describe("OngoingGamesContainer", () => {
       fetchOngoingGamesFn.mockClear();
       fetchOngoingGamesFn.mockReturnValue(fetchOngoingGamesReturnedValue);
 
-      TestRenderer.act(() => {
-        TestRenderer.create(<OngoingGamesContainer />);
-      });
+      TestRenderer.create(<OngoingGamesContainer />);
 
       expect(fetchOngoingGamesFn).toBeCalledTimes(1);
       expect(fetchOngoingGamesFn).toBeCalledWith();
--- src/test-utils/data-sample/state.ts
+++ src/test-utils/data-sample/state.ts
@@ -22,3 +22,37 @@ export const defaultState: RootState = {
     games: {},
   },
 };
+
+export const stateWithOngoingGames: RootState = {
+  currentUser: {
+    userId: null,
+    isLoading: false,
+    error: null,
+  },
+  authModal: {
+    isAuthModalVisible: false,
+  },
+  challengeAiModal: {
+    isChallengeAiModalVisible: false,
+  },
+  ongoingGames: {
+    items: [1],
+    isLoading: false,
+    error: null,
+  },
+  entities: {
+    users: {},
+    games: {
+      "1": {
+        id: 1,
+        initialFen: "startpos",
+        wtime: 300000,
+        btime: 300000,
+        moves: "",
+        status: "started",
+        white: null,
+        black: null,
+      },
+    },
+  },
+};
