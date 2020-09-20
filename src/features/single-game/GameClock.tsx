200355f71357397f1b1fdf12526c51acf6b89552
ildar <ildar.icoosoft@gmail.com>
2020-09-20 21:57:39 +0500

    interm

--- src/features/single-game/GameClock.tsx
+++ src/features/single-game/GameClock.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import moment from "moment";
 import Game from "../../interfaces/Game";
 import { PieceColor } from "../../types/PieceColor";
@@ -21,7 +21,26 @@ export const GameClock: FC<GameClockProps> = ({ game, color = "white" }) => {
     msec = game.btime;
   }
 
-  const time = moment().startOf("day").milliseconds(msec).format("mm : ss");
+  const [timePassed, setTimePassed] = useState(0);
+
+  useEffect(() => {
+    if (game.status !== "started") {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      setTimePassed((timePassed) => timePassed + 1000);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  const time = moment()
+    .startOf("day")
+    .milliseconds(msec - timePassed)
+    .format("mm : ss");
 
   return <div data-testid="time">{time}</div>;
 };
--- src/features/single-game/__tests__/GameClock.test.tsx
+++ src/features/single-game/__tests__/GameClock.test.tsx
@@ -1,9 +1,18 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import { render, act } from "@testing-library/react";
+import React, { useEffect } from "react";
 import { GameClock } from "../GameClock";
-import { gameSample } from "../../../test-utils/data-sample/game";
+import {
+  gameSample,
+  gameWithCheckmateByWhiteSample,
+} from "../../../test-utils/data-sample/game";
+
+jest.useFakeTimers();
 
 describe("GameClock", () => {
+  beforeEach(() => {
+    (useEffect as jest.Mock).mockReset();
+  });
+
   describe("DOM structure", () => {
     it("should display time", () => {
       const { rerender, getByTestId } = render(<GameClock game={gameSample} />);
@@ -12,6 +21,40 @@ describe("GameClock", () => {
 
       rerender(<GameClock game={gameSample} color={"black"} />);
       expect(getByTestId("time").innerHTML).toBe("06 : 05");
+    });
+
+    it("should update time if game is playing", () => {
+      (useEffect as jest.Mock).mockImplementationOnce((cb) => cb());
+      (useEffect as jest.Mock).mockImplementationOnce((cb) => cb());
+
+      const { getByTestId } = render(<GameClock game={gameSample} />);
+
+      expect(getByTestId("time").innerHTML).toBe("05 : 10");
+
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+
+      expect(getByTestId("time").innerHTML).toBe("05 : 08");
+    });
+
+    it("should not update time if game is not playing", () => {
+      (useEffect as jest.Mock).mockImplementationOnce((cb) => cb());
+
+      const { getByTestId } = render(
+        <GameClock game={gameWithCheckmateByWhiteSample} />
+      );
+
+      expect(getByTestId("time").innerHTML).toBe("05 : 00");
+
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+
+      expect(getByTestId("time").innerHTML).toBe("05 : 00");
     });
 
     it("should contain nothing if no game", () => {
