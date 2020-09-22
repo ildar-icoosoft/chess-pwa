d9873c07134e012d56da965b2298a57c279be8a9
ildar <ildar.icoosoft@gmail.com>
2020-09-22 20:19:43 +0500

    interm

--- src/features/single-game/GameControlPanelBottomToolbar.tsx
+++ src/features/single-game/GameControlPanelBottomToolbar.tsx
@@ -3,20 +3,42 @@ import { FC } from "react";
 
 export interface GameControlPanelBottomToolbarProps {
   onAbortGame?(): void;
+  onOfferDraw?(): void;
+  onResignGame?(): void;
 }
 
 export const GameControlPanelBottomToolbar: FC<GameControlPanelBottomToolbarProps> = ({
   onAbortGame,
+  onOfferDraw,
+  onResignGame,
 }) => {
   const handleAbortGame = useCallback(() => {
     if (onAbortGame) {
       onAbortGame();
     }
   }, [onAbortGame]);
+  const handleOfferDraw = useCallback(() => {
+    if (onOfferDraw) {
+      onOfferDraw();
+    }
+  }, [onOfferDraw]);
+  const handleResignGame = useCallback(() => {
+    if (onResignGame) {
+      onResignGame();
+    }
+  }, [onResignGame]);
 
   return (
-    <button data-testid={"abort-game-btn"} onClick={handleAbortGame}>
-      Abort
-    </button>
+    <>
+      <button data-testid={"abort-game-btn"} onClick={handleAbortGame}>
+        Abort
+      </button>
+      <button data-testid={"offer-draw-btn"} onClick={handleOfferDraw}>
+        Offer a draw
+      </button>
+      <button data-testid={"resign-game-btn"} onClick={handleResignGame}>
+        Resign
+      </button>
+    </>
   );
 };
--- src/features/single-game/__tests__/GameControlPanelBottomToolbar.test.tsx
+++ src/features/single-game/__tests__/GameControlPanelBottomToolbar.test.tsx
@@ -17,5 +17,31 @@ describe("GameMeta", () => {
       expect(onAbortGame).toBeCalledTimes(1);
       expect(onAbortGame).toBeCalledWith();
     });
+
+    it("onOfferDraw", () => {
+      const onOfferDraw = jest.fn();
+
+      const { getByTestId } = render(
+        <GameControlPanelBottomToolbar onOfferDraw={onOfferDraw} />
+      );
+
+      fireEvent.click(getByTestId("offer-draw-btn"));
+
+      expect(onOfferDraw).toBeCalledTimes(1);
+      expect(onOfferDraw).toBeCalledWith();
+    });
+
+    it("onResignGame", () => {
+      const onResignGame = jest.fn();
+
+      const { getByTestId } = render(
+        <GameControlPanelBottomToolbar onResignGame={onResignGame} />
+      );
+
+      fireEvent.click(getByTestId("resign-game-btn"));
+
+      expect(onResignGame).toBeCalledTimes(1);
+      expect(onResignGame).toBeCalledWith();
+    });
   });
 });
