492ab362948d533fddbc18a0aab2a6b0fff80b70
ildar <ildar.icoosoft@gmail.com>
2020-09-28 00:59:29 +0500

    interm

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -27,46 +27,44 @@ export const Header: FC<HeaderProps> = ({
   }, [onShowAuthModal]);
 
   return (
-    <header className="blog-header py-3">
-      <div className="row flex-nowrap justify-content-between align-items-center">
-        <div className="offset-4 col-4 text-center">
-          <Link className="blog-header-logo text-dark" to="/">
-            Chess PWA
-          </Link>
-        </div>
-        <div className="col-4 d-flex justify-content-end align-items-center">
-          {currentUser ? (
-            <>
-              <div className="mr-2">Hi, {currentUser.fullName}</div>
-              <Button
-                className="btn btn-sm btn-danger mr-2"
-                data-testid="logout-btn"
-                onClick={handleLogout}
-              >
-                Logout
-              </Button>
-            </>
-          ) : (
+    <header className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <div className="offset-4 col-4 text-center">
+        <Link className="blog-header-logo text-dark" to="/">
+          Chess PWA
+        </Link>
+      </div>
+      <div className="col-4 d-flex justify-content-end align-items-center">
+        {currentUser ? (
+          <>
+            <div className="mr-3">Hi, {currentUser.fullName}</div>
             <Button
-              className="btn btn-sm btn-success mr-2"
-              data-testid="login-btn"
-              onClick={handleShowAuthModal}
+              className="btn btn-sm btn-danger mr-3"
+              data-testid="logout-btn"
+              onClick={handleLogout}
             >
-              Login / Register
+              Logout
             </Button>
-          )}
-
-          <a
-            href="https://github.com/ildar-icoosoft/chess-pwa"
-            target="_blank"
-            rel="noopener noreferrer"
+          </>
+        ) : (
+          <Button
+            className="btn btn-sm btn-success mr-3"
+            data-testid="login-btn"
+            onClick={handleShowAuthModal}
           >
-            <img
-              src={process.env.PUBLIC_URL + "/imgs/GitHub-Mark-32px.png"}
-              alt=""
-            />
-          </a>
-        </div>
+            Login / Register
+          </Button>
+        )}
+
+        <a
+          href="https://github.com/ildar-icoosoft/chess-pwa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={process.env.PUBLIC_URL + "/imgs/GitHub-Mark-32px.png"}
+            alt=""
+          />
+        </a>
       </div>
     </header>
   );
