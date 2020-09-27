a9bcaea21b1acb4df07a5e5cae66cec6976a59d4
ildar <ildar.icoosoft@gmail.com>
2020-09-28 03:51:42 +0500

    interm

---
+++ src/__mocks__/react-bootstrap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as reactBootstrap from "react-bootstrap";
+
+reactBootstrap.Modal = ({ children }) => {
+  return <>{children}</>;
+};
+
+reactBootstrap.Modal.Body = () => null;
+reactBootstrap.Modal.Header = () => null;
+reactBootstrap.Modal.Title = () => null;
+reactBootstrap.Modal.Footer = () => null;
+reactBootstrap.Modal.Dialog = () => null;
+
+module.exports = reactBootstrap;
--- src/features/home-page/ChallengeAiModal.tsx
+++ src/features/home-page/ChallengeAiModal.tsx
@@ -1,7 +1,27 @@
-import React, { FC, useCallback } from "react";
+import React, { FC } from "react";
+import { Modal } from "react-bootstrap";
+import ChallengeAiFormContainer from "../challenge-ai-modal/ChallengeAiFormContainer";
 
-export interface ChallengeAiModalProps {}
+export interface ChallengeAiModalProps {
+  show?: boolean;
+  onHide?(): void;
+}
 
-export const ChallengeAiModal: FC<ChallengeAiModalProps> = () => {
-  return null;
+export const ChallengeAiModal: FC<ChallengeAiModalProps> = ({
+  show = false,
+  onHide,
+}) => {
+  return (
+    <div>
+      <Modal show={show} onHide={onHide} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Play with the computer</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <ChallengeAiFormContainer />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 };
