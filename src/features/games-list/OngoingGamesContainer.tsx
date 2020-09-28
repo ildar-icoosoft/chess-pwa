5804a54207d504bcd1355a40f228f86d0a3e8d29
ildar <ildar.icoosoft@gmail.com>
2020-09-29 02:19:32 +0500

    filter ongoing games

--- src/features/games-list/OngoingGamesContainer.tsx
+++ src/features/games-list/OngoingGamesContainer.tsx
@@ -4,10 +4,15 @@ import { useSelector } from "react-redux";
 import { GamePreviewsList } from "./GamePreviewsList";
 import { RootState } from "../../app/rootReducer";
 import gameSchema from "../../normalizr/schemas/gameSchema";
+import Game from "../../interfaces/Game";
 
 const OngoingGamesContainer: FC<unknown> = () => {
   const items = useSelector((state: RootState) =>
-    denormalize(state.gamesList.items, [gameSchema], state.entities)
+    denormalize(
+      Object.keys(state.entities.games),
+      [gameSchema],
+      state.entities
+    ).filter((game: Game) => game.status === "started")
   );
 
   return <GamePreviewsList games={items} />;
