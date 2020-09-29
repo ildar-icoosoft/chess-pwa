65f32a681cae85a35b57875841c9eee474465c1f
ildar <ildar.icoosoft@gmail.com>
2020-09-29 14:54:12 +0500

    interm

--- src/features/games-list/GamePreviewsListItem.tsx
+++ src/features/games-list/GamePreviewsListItem.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
+import cx from "classnames";
 import Game from "../../interfaces/Game";
 import { Link } from "react-router-dom";
 import { Board } from "ii-react-chessboard";
 import makeChessInstance from "../../utils/makeChessInstance";
+import css from "./GamePreviewsListItem.module.scss";
 
 export interface GamePreviewsListItemProps {
   game?: Game;
@@ -18,8 +20,20 @@ export const GamePreviewsListItem: FC<GamePreviewsListItemProps> = ({
   const fen: string = makeChessInstance(game).fen();
 
   return (
-    <Link to={`/game/${game.id}`}>
-      <Board position={fen} viewOnly={false} width={240} />
-    </Link>
+    <div className={css.gamePreview}>
+      <div className={css.player}>
+        <span className={css.user}>Mr Nobody</span>
+        <span className={cx(css.clock, css.clockRun)}>02:14</span>
+      </div>
+      <div className="pb-2">
+        <Link to={`/game/${game.id}`}>
+          <Board position={fen} viewOnly={false} width={240} />
+        </Link>
+      </div>
+      <div className={css.player}>
+        <span className={css.user}>Mr Nobody</span>
+        <span className={css.clock}>03:15</span>
+      </div>
+    </div>
   );
 };
