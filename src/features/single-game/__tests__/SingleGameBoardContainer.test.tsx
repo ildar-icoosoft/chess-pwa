9c60ac6f7bad981fe22801a0dfda4ea9e6f0ff6a
ildar <ildar.icoosoft@gmail.com>
2020-09-30 04:24:20 +0500

    interm

--- src/features/single-game/__tests__/SingleGameBoardContainer.test.tsx
+++ src/features/single-game/__tests__/SingleGameBoardContainer.test.tsx
@@ -1,10 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import { stateWithDataSample } from "../../../test-utils/data-sample/state";
+import {
+  stateWithDataSample,
+  stateWithDataSample2,
+  stateWithDataSample3,
+} from "../../../test-utils/data-sample/state";
 import React, { useEffect } from "react";
 import mountTest from "../../../test-utils/mountTest";
 import { SingleGameBoardContainer } from "../SingleGameBoardContainer";
 import TestRenderer from "react-test-renderer";
 import { SingleGameBoard } from "../SingleGameBoard";
+import { makeMove } from "../../move/moveSlice";
+
+jest.mock("../../move/moveSlice");
 
 describe("SingleGameBoardContainer", () => {
   beforeEach(() => {
@@ -60,6 +67,115 @@ describe("SingleGameBoardContainer", () => {
           winner: null,
         });
       });
+
+      it("currentUser", async () => {
+        const testRenderer = TestRenderer.create(
+          <SingleGameBoardContainer id={1} />
+        );
+        const testInstance = testRenderer.root;
+
+        const singleGameBoard = testInstance.findByType(SingleGameBoard);
+
+        expect(singleGameBoard.props.currentUser).toEqual({
+          id: 1,
+          fullName: "Thomas Miller",
+        });
+
+        (useSelector as jest.Mock).mockImplementation((cb) =>
+          cb(stateWithDataSample2)
+        );
+
+        testRenderer.update(<SingleGameBoardContainer id={1} />);
+
+        expect(singleGameBoard.props.currentUser).toBeUndefined();
+      });
+
+      it("isFlipped", async () => {
+        const testRenderer = TestRenderer.create(
+          <SingleGameBoardContainer id={1} />
+        );
+        const testInstance = testRenderer.root;
+
+        const singleGameBoard = testInstance.findByType(SingleGameBoard);
+
+        expect(singleGameBoard.props.isFlipped).toBeFalsy();
+
+        (useSelector as jest.Mock).mockImplementation((cb) =>
+          cb(stateWithDataSample2)
+        );
+
+        testRenderer.update(<SingleGameBoardContainer id={1} />);
+        expect(singleGameBoard.props.isFlipped).toBeFalsy();
+
+        (useSelector as jest.Mock).mockImplementation((cb) =>
+          cb(stateWithDataSample3)
+        );
+
+        testRenderer.update(<SingleGameBoardContainer id={1} />);
+        expect(singleGameBoard.props.isFlipped).toBeTruthy();
+      });
+
+      it("rewindToMoveIndex", async () => {
+        const testRenderer = TestRenderer.create(
+          <SingleGameBoardContainer id={1} />
+        );
+        const testInstance = testRenderer.root;
+
+        const singleGameBoard = testInstance.findByType(SingleGameBoard);
+
+        expect(singleGameBoard.props.rewindToMoveIndex).toBeNull();
+
+        (useSelector as jest.Mock).mockImplementation((cb) =>
+          cb(stateWithDataSample2)
+        );
+
+        testRenderer.update(<SingleGameBoardContainer id={1} />);
+        expect(singleGameBoard.props.rewindToMoveIndex).toBe(2);
+
+        (useSelector as jest.Mock).mockImplementation((cb) =>
+          cb(stateWithDataSample3)
+        );
+
+        testRenderer.update(<SingleGameBoardContainer id={1} />);
+        expect(singleGameBoard.props.rewindToMoveIndex).toBe(0);
+      });
+    });
+  });
+
+  describe("dispatch() calls", () => {
+    it("should call dispatch(makeMove())", () => {
+      const dispatch = useDispatch<jest.Mock>();
+      const makeMoveReturnedValue = Symbol("makeMove");
+
+      const testRenderer = TestRenderer.create(
+        <SingleGameBoardContainer id={1} />
+      );
+      const testInstance = testRenderer.root;
+
+      const singleGameBoard = testInstance.findByType(SingleGameBoard);
+
+      const makeMoveFn = makeMove as jest.Mock;
+      makeMoveFn.mockClear();
+      makeMoveFn.mockReturnValue(makeMoveReturnedValue);
+
+      TestRenderer.act(() => {
+        singleGameBoard.props.onMove({
+          from: "e2",
+          to: "e4",
+        });
+      });
+
+      expect(makeMoveFn).toBeCalledTimes(1);
+      expect(makeMoveFn).toBeCalledWith(1, "e2e4");
+
+      expect(dispatch).toBeCalledWith(makeMoveReturnedValue);
     });
   });
 });
+
+/*
+currentUser,
+  game,
+  isFlipped = false,
+  onMove,
+  rewindToMoveIndex = null,*/
