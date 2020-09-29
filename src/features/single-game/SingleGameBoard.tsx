0ea8119830b6c25b8f72dca68879f992861520e8
ildar <ildar.icoosoft@gmail.com>
2020-09-30 02:06:20 +0500

    interm

--- src/features/single-game/SingleGameBoard.tsx
+++ src/features/single-game/SingleGameBoard.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from "react";
 import Game from "../../interfaces/Game";
+import { Board } from "ii-react-chessboard";
+import { ChessInstance } from "chess.js";
+import makeChessInstance from "../../utils/makeChessInstance";
 
 export interface SingleGameBoardProps {
   game?: Game;
+  rewindToMoveIndex?: number | null;
 }
 
-export const SingleGameBoard: FC<SingleGameBoardProps> = () => {
-  return null;
+export const SingleGameBoard: FC<SingleGameBoardProps> = ({
+  game,
+  rewindToMoveIndex,
+}) => {
+  if (!game) {
+    return null;
+  }
+
+  const chessWithAllMoves: ChessInstance = makeChessInstance(game);
+
+  const chess: ChessInstance =
+    rewindToMoveIndex === null
+      ? chessWithAllMoves
+      : makeChessInstance(game, rewindToMoveIndex);
+
+  const fen: string = chess.fen();
+
+  const check: boolean = chess.in_check();
+
+  return (
+    <Board allowMarkers check={check} clickable draggable position={fen} />
+  );
 };
