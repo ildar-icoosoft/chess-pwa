213d4d7da5869dd659be51963c2e72f63cd76984
ildar <ildar.icoosoft@gmail.com>
2020-10-09 22:25:05 +0500

    interm

--- src/features/single-game/__tests__/GameControlPanelWrapper.test.tsx
+++ src/features/single-game/__tests__/GameControlPanelWrapper.test.tsx
@@ -492,7 +492,7 @@ describe("GameControlPanelWrapper", () => {
         gameControlPanel.props.onRewindToPrevMove();
 
         expect(onRewindToMove).toBeCalledTimes(1);
-        expect(onRewindToMove).toBeCalledWith(2);
+        expect(onRewindToMove).toBeCalledWith(3);
 
         testRenderer.update(
           <GameControlPanelWrapper
@@ -532,7 +532,7 @@ describe("GameControlPanelWrapper", () => {
         testRenderer.update(
           <GameControlPanelWrapper
             game={gameWithMovesSample}
-            rewindToMoveIndex={2}
+            rewindToMoveIndex={3}
             onRewindToMove={onRewindToMove}
           />
         );
