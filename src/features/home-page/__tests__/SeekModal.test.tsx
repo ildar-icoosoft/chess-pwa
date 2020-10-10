eb2ff021599c71b17fbb80a15a5bee528fd7af8f
ildar <ildar.icoosoft@gmail.com>
2020-10-11 02:05:38 +0500

    interm

---
+++ src/features/home-page/__tests__/SeekModal.test.tsx
@@ -0,0 +1,46 @@
+import TestRenderer from "react-test-renderer";
+import React from "react";
+import { Modal } from "react-bootstrap";
+import mountTest from "../../../test-utils/mountTest";
+import { SeekModal } from "../SeekModal";
+
+describe("SeekModal", () => {
+  mountTest(SeekModal);
+
+  describe("children components", () => {
+    it("contains Modal", () => {
+      const testRenderer = TestRenderer.create(<SeekModal />);
+      const testInstance = testRenderer.root;
+
+      expect(testInstance.findAllByType(Modal).length).toBe(1);
+    });
+  });
+
+  describe("children components props", () => {
+    describe("Modal", () => {
+      it("show", () => {
+        const testRenderer = TestRenderer.create(<SeekModal />);
+        const testInstance = testRenderer.root;
+
+        const modal = testInstance.findByType(Modal);
+
+        expect(modal.props.show).toBeFalsy();
+
+        testRenderer.update(<SeekModal show />);
+
+        expect(modal.props.show).toBeTruthy();
+      });
+
+      it("onHide", () => {
+        const onHide = jest.fn();
+
+        const testRenderer = TestRenderer.create(<SeekModal onHide={onHide} />);
+        const testInstance = testRenderer.root;
+
+        const modal = testInstance.findByType(Modal);
+
+        expect(modal.props.onHide).toBe(onHide);
+      });
+    });
+  });
+});
