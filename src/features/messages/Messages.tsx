e861dfd1d2d64fcdfbce1d4092496cf60f682982
ildar <ildar.icoosoft@gmail.com>
2020-10-14 02:39:00 +0500

    interm

--- src/features/messages/Messages.tsx
+++ src/features/messages/Messages.tsx
@@ -1,9 +1,33 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import { MessageItemState } from "./messagesSlice";
+import { Toast } from "react-bootstrap";
 
 export interface MessagesProps {
   items?: MessageItemState[];
   onHideMessage?(messageId: string): void;
 }
 
-export const Messages: FC<MessagesProps> = () => null;
+export const Messages: FC<MessagesProps> = ({ items = [], onHideMessage }) => {
+  const makeHideMessageCallback = (messageId: string) => () => {
+    if (onHideMessage) {
+      onHideMessage(messageId);
+    }
+  };
+
+  return (
+    <>
+      {items.map((item) => (
+        <Toast
+          key={item.id}
+          onClose={makeHideMessageCallback(item.id)}
+          show
+          delay={3000}
+          autohide
+          animation={false}
+        >
+          <Toast.Body>{item.body}</Toast.Body>
+        </Toast>
+      ))}
+    </>
+  );
+};
