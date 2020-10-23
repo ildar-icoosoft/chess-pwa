77bfb7c88af9531cb67c1f2df6c41588ba191b16
ildar <ildar.icoosoft@gmail.com>
2020-10-24 01:47:53 +0500

    interm

--- src/features/chat/ChatMessagesListItem.tsx
+++ src/features/chat/ChatMessagesListItem.tsx
@@ -1,10 +1,30 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import cx from "classnames";
 import { ChatMessage } from "../../interfaces/ChatMessage";
-import { Seek } from "../../interfaces/Seek";
+import css from "./ChatMessagesListItem.module.scss";
 
 export interface ChatMessagesListItemProps {
   message?: ChatMessage;
   currentUserId?: number | null;
 }
 
-export const ChatMessagesListItem: FC<ChatMessagesListItemProps> = () => null;
+export const ChatMessagesListItem: FC<ChatMessagesListItemProps> = ({
+  message,
+  currentUserId = null,
+}) => {
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <div
+      data-testid="message-wrapper"
+      className={cx({
+        [css.currentUserMessage]: message.createdBy.id === currentUserId,
+      })}
+    >
+      <div data-testid="user-name">{message.createdBy.fullName}</div>
+      <div data-testid="message-text">{message.text}</div>
+    </div>
+  );
+};
