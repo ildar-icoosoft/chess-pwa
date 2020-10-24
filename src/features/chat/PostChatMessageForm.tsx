939941d35af53617eaa4e444638544a967b205c8
ildar <ildar.icoosoft@gmail.com>
2020-10-24 09:19:09 +0500

    interm

--- src/features/chat/PostChatMessageForm.tsx
+++ src/features/chat/PostChatMessageForm.tsx
@@ -2,9 +2,10 @@
 
 import React, { FC, FormEvent } from "react";
 import { Formik, FormikHelpers } from "formik";
-import { Button, Form, Spinner } from "react-bootstrap";
-import cx from "classnames";
+import { Button, Col, Form, Row, Spinner } from "react-bootstrap";
 import css from "./PostChatMessageForm.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 export interface PostChatMessageFormData {
   text: string;
@@ -48,30 +49,38 @@ export const PostChatMessageForm: FC<PostChatMessageFormProps> = ({
           onSubmit={(e) => handleSubmit(e as FormEvent<HTMLFormElement>)}
           onChange={() => setStatus("")}
         >
-          <Form.Group>
-            <Form.Control
-              name="text"
-              autoComplete="off"
-              placeholder="Please be nice in the chat"
-              onBlur={handleBlur}
-              value={values.text}
-              onChange={handleChange}
-            />
-          </Form.Group>
-
-          <Button disabled={isSubmitting || values.text === ""} type="submit">
-            {isSubmitting && (
-              <Spinner
-                animation="border"
-                data-testid="submit-btn-spinner"
-                size="sm"
-                className="mr-2"
+          <div className="d-flex">
+            <div className="flex-grow-1 mr-2">
+              <Form.Control
+                className={css.textInput}
+                name="text"
+                autoComplete="off"
+                placeholder="Please be nice in the chat"
+                onBlur={handleBlur}
+                value={values.text}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Button
+                disabled={isSubmitting || values.text === ""}
+                type="submit"
               >
-                <span className="sr-only">Sending...</span>
-              </Spinner>
-            )}
-            Send
-          </Button>
+                {isSubmitting && (
+                  <Spinner
+                    animation="border"
+                    data-testid="submit-btn-spinner"
+                    size="sm"
+                    className="mr-2"
+                  >
+                    <span className="sr-only">Sending...</span>
+                  </Spinner>
+                )}
+                <FontAwesomeIcon icon={faPaperPlane} />
+                <span className="sr-only">Offer a draw</span>
+              </Button>
+            </div>
+          </div>
         </Form>
       )}
     </Formik>
