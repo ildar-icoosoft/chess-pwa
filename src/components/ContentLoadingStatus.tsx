b1c380af7129e1c7194b67f0267eded28089810f
ildar <ildar.icoosoft@gmail.com>
2020-10-30 02:01:12 +0500

    interm

--- src/components/ContentLoadingStatus.tsx
+++ src/components/ContentLoadingStatus.tsx
@@ -11,46 +11,54 @@ export interface ContentLoadingStatusProps {
 
 const defaultEmptyContentMessage = "content is empty";
 
-export const ContentLoadingStatus: FC<ContentLoadingStatusProps> = ({
-  emptyContentMessage = defaultEmptyContentMessage,
-  isEmpty = false,
-  isLoading = false,
-  error = null,
-  showEmptyContentMessage = true,
-}) => {
-  if (isEmpty && !isLoading && !error && showEmptyContentMessage) {
-    return (
-      <div
-        className="alert alert-warning text-center"
-        role="alert"
-        data-testid="empty-content-message"
-      >
-        {emptyContentMessage}
-      </div>
-    );
-  }
+export const ContentLoadingStatus: FC<ContentLoadingStatusProps> = React.memo(
+  ({
+    emptyContentMessage = defaultEmptyContentMessage,
+    isEmpty = false,
+    isLoading = false,
+    error = null,
+    showEmptyContentMessage = true,
+  }) => {
+    console.log("ContentLoadingStatus");
 
-  if (isLoading && isEmpty) {
-    return (
-      <div className="text-center">
-        <Spinner animation="border" role="status" data-testid="loading-spinner">
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      </div>
-    );
-  }
+    if (isEmpty && !isLoading && !error && showEmptyContentMessage) {
+      return (
+        <div
+          className="alert alert-warning text-center"
+          role="alert"
+          data-testid="empty-content-message"
+        >
+          {emptyContentMessage}
+        </div>
+      );
+    }
 
-  if (error) {
-    return (
-      <div
-        className="alert alert-danger text-center"
-        role="alert"
-        data-testid="error-message"
-      >
-        {error}
-      </div>
-    );
-  }
+    if (isLoading && isEmpty) {
+      return (
+        <div className="text-center">
+          <Spinner
+            animation="border"
+            role="status"
+            data-testid="loading-spinner"
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      );
+    }
 
-  return null;
-};
+    if (error) {
+      return (
+        <div
+          className="alert alert-danger text-center"
+          role="alert"
+          data-testid="error-message"
+        >
+          {error}
+        </div>
+      );
+    }
+
+    return null;
+  }
+);
