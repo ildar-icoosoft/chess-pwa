8bdfbe209f71e9b54dc17047ef729f99ba482603
ildar <ildar.icoosoft@gmail.com>
2020-10-31 01:22:43 +0500

    interm

--- src/services/ioClient.ts
+++ src/services/ioClient.ts
@@ -5,4 +5,18 @@ const ioClient = sailsIOClient(socketIOClient);
 
 ioClient.sails.url = process.env.REACT_APP_BACKEND_HOST;
 
+ioClient.sails.reconnection = true;
+
+ioClient.socket.on("connect_timeout", () => {
+  alert("connect_timeout");
+});
+
+ioClient.socket.on("reconnect", () => {
+  alert("reconnect");
+});
+
+ioClient.socket.on("disconnect", () => {
+  alert("disconnect");
+});
+
 export default ioClient;
